25fb26149f35bcfd6e2ff574b6bd7b711ce0902a
Evelina <Evelinabum123@gmail.com>
2025-04-23 15:40:24 +0300

    get-in-touch

--- js/sections/get-in-touch.js
+++ js/sections/get-in-touch.js
@@ -1,11 +1,22 @@
 export function getInTouch() {
-    const HTML = `
-        <section class="container">
-            <div class="row">
-                <div class="col-12">
-                    HERO CONTENT
-                </div>
-            </div>
-        </section>`;
-    document.body.insertAdjacentHTML('beforeend', HTML);
-}
+  const html = `
+    
+<section class="section-bg">
+  <div class="contact-box">
+  <div class="contact-left">
+  <h4 class="get-in">Get It Touch</h4>
+  <h1 class="title">Have a Project on Your Mind</h1>
+  <button class="btn">Contact Me +</button>
+  </div>
+  <div class="contact-right">
+  <div class="bg"></div>
+  <img class="body-bg" src="./img/body.png" alt="Body">
+  <img class="wmn" src="./img/wmn.png" alt="Woman" >
+   </div>
+  </div>
+ </div>
+</section>
+ `;
+  
+    document.body.insertAdjacentHTML('beforeend', html);
+  }
