60c8faf9f976321b78d671725f5d6bcb7930e546
mindaugas.samsonas <mindaugas.samsonas@studentas.bit.lt>
2025-04-28 22:57:13 +0300

    commiting colors

--- js/sections/pricing.js
+++ js/sections/pricing.js
@@ -3,9 +3,22 @@ export function pricing() {
         <section class="container">
             <div class="row">
                 <div class="col-12">
+                    <h3>Pricing</h3>
+                    <h1>Reasonable Pricing Plan</h1>
+                    <p>Labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo
+viverra maecenas accumsan lacus vel facilisis.</p>
+                    <div class="card-line">
                     
+                    </div>
                 </div>
             </div>
         </section>`;
     document.body.insertAdjacentHTML('beforeend', HTML);
 }
+
+//pasirasyti funkcija kuri sugeneruoja viena kortele
+function cardGenerator()
+{
+    
+}
+
