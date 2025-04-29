28c817484e4e19730b4a6e671194be25a5d087ef
Mantas Kukulskis <8mantas8@gmail.com>
2025-04-29 08:31:45 +0300

    addet data folder. portfolio css & blog css changed px to rem

---
+++ js/data/portfolio-services.js
@@ -0,0 +1,26 @@
+export const services = [
+    {
+      category: "Product Design",
+      title: "The Services Provide for Web Design",
+      description: "Disse ultrices gravida. Risus comod viverra maecenas accumsan lacus vel facilisis.",
+      image: "https://html.themexriver.com/Saasio/assets/img/port-img-31/port-1.png",
+    },
+    {
+      category: "App Development",
+      title: "The Services Provide for App Development",
+      description: "Disse ultrices gravida. Risus comod viverra maecenas accumsan lacus vel facilisis.",
+      image: "https://html.themexriver.com/Saasio/assets/img/port-img-31/port-2.png",
+    },
+    {
+      category: "Web Development",
+      title: "The Services Provide for Web Development",
+      description: "Disse ultrices gravida. Risus comod viverra maecenas accumsan lacus vel facilisis.",
+      image: "https://html.themexriver.com/Saasio/assets/img/port-img-31/port-3.png",
+    },
+    {
+      category: "Marketing",
+      title: "The Services Provide for Marketing",
+      description: "Disse ultrices gravida. Risus comod viverra maecenas accumsan lacus vel facilisis.",
+      image: "https://html.themexriver.com/Saasio/assets/img/port-img-31/port-4.png",
+    }
+  ];
